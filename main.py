c44fc49db609c52bf8dcfa6157e0f805ac1020ba
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-09-12 16:48:12 +0200

    File import

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-File
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img1=mpimg.imread('33505.jpg')
+img2=mpimg.imread('33505 - Copy.jpg')
+print(img2)
+imgplot = plt.imshow(img2)
