1b15f4be6e2c7314d8513cb25c9924f829462b2e
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-09-26 20:02:28 +0200

    finding the diff with normalization

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ img2 = cv2.imread('33505 - Copy.jpg', cv2.IMREAD_GRAYSCALE)
 
 diff_cod = cv2.subtract(img1, img2)
 
+coords = np.argwhere(diff_cod > 0)
+coords_list = coords.tolist()
+print(coords_list[:10])
 imS = cv2.resize(img1, (960, 540))
-cv2.imshow('image',imS)
+cv2.imshow('image', imS)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
