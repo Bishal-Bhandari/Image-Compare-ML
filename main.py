3e103982a61841e363ac9d41a2923f5faf1ec4ee
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-09-26 19:51:57 +0200

    finding the diff with normalization

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 img1 = cv2.imread('33505.jpg', cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread('33505 - Copy.jpg', cv2.IMREAD_GRAYSCALE)
 
+diff_cod = cv2.subtract(img1, img2)
+
 imS = cv2.resize(img1, (960, 540))
 cv2.imshow('image',imS)
 cv2.waitKey(0)
